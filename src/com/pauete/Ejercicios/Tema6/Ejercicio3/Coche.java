ca16a411f31302b3be8eb50b6bf51651a9e14b11
Pau Salanova Blanco <pauete.sb@gmail.com>
2022-02-06 21:08:15 +0100

    Ejercicio final t6
    OwO

--- src/com/pauete/Ejercicios/Tema6/Ejercicio3/Coche.java
+++ src/com/pauete/Ejercicios/Tema6/Ejercicio3/Coche.java
@@ -13,20 +13,39 @@ public class Coche {
     private int numPuertas = 3;
     private int numPlazas = 5;
 
-    public Coche() {
+    //Ejercicio Final
+    /**
+     * El número actual de coches
+     */
+    public static int numcoches = 0;
+    /**
+     * El máximo de coches que se pueden fabricar
+     */
+    public static final int MAX_COCHES = 5;
+    /**
+     * Una colección de coches fabricados
+     */
+    public static Coche[] coches = new Coche[MAX_COCHES];
 
+    public Coche() {
+        coches[numcoches] = this;
+        numcoches ++;
+        this.matricula = matAleatiora();
     }
 
     public Coche(String matricula) {
+        this();
         this.matricula = matricula;
     }
 
     public Coche(int numPuertas, int numPlazas) {
+        this();
         this.setNumPlazas(numPlazas);
         this.setNumPuertas(numPuertas);
     }
 
     public Coche(String marca, String modelo, String color) {
+        this();
         this.setColor(color);
         this.setMarca(marca);
         this.setModelo(modelo);
@@ -99,13 +118,11 @@ public class Coche {
     public void avanzar(int km) {
         boolean avanza = km > 0;
         if (avanza) { this.setKilometros(this.getKilometros() + km); }
-        JOptionPane.showMessageDialog(null, "Has avanzado " + (avanza ? km : 0) + "km");
     }
 
     public void tunear() {
         this.setKilometros(0);
         this.setTechoSolar(true);
-        JOptionPane.showMessageDialog(null, "Ahora tienes un coche nuevecito");
     }
 
     public void tunear(String color) {
@@ -115,7 +132,35 @@ public class Coche {
 
     public void matricular(String matricula) {
         this.matricula = matricula;
-        JOptionPane.showMessageDialog(null, "Has matriculado el coche como " + this.getMatricula());
+    }
+
+    /**
+     * Genera una matrícula pseudoaleatoria de 5 dígitos
+     * @return una matrícula aleatoria.
+     */
+    private static String matAleatiora() {
+        StringBuilder matricula = new StringBuilder();
+        for (int i=0;i<5;i++) {
+            matricula.append(String.valueOf( ((int) Math.floor(Math.random() * 10))));
+        }
+        return matricula.toString();
+    }
+
+    /**
+     * Busca un coche entre todos los fabricados a partir de su matrícula
+     * @param matricula la matrícula del coche
+     * @return el coche
+     * @throws Exception si no hay ningún coche con esa matrícula
+     */
+    public static Coche buscaCoche(String matricula) throws Exception {
+        for (Coche coche : coches) {
+            if (coche != null) {
+                if (coche.getMatricula().equals(matricula)) {
+                    return coche;
+                }
+            }
+        }
+        throw new Exception("Esa matrícula no pertenece a ningún coche");
     }
 
 }
--- src/com/pauete/Ejercicios/Tema6/Ejercicio3/Fabrica.java
+++ src/com/pauete/Ejercicios/Tema6/Ejercicio3/Fabrica.java
@@ -4,46 +4,130 @@ import javax.swing.*;
 
 public class Fabrica {
     public static void main(String[] args) {
-        //Coche 1
-        Coche car1 = new Coche();
-        car1.setMatricula("1234-DF");
-        car1.setModelo("Toledo");
-        car1.setColor("azul");
-        car1.avanzar(200);
 
-        //Coche 2
-        Coche car2 = new Coche("5678-AG");
-        car2.setMarca("Fiat");
-        car2.setModelo("Uno");
-        car2.tunear("rojo");
-        car2.setTechoSolar(true);
-        car2.avanzar(300);
-        car2.setNumPlazas(2);
+        int option = 0;
+        do {
+            try {
+                option = Integer.parseInt(JOptionPane.showInputDialog("""
+                        Fábrica de Coches de PaueteKKUET. Selecciona una de las siguientes opciones:
+                        1 - Fabricar coche (conociendo matrícula)
+                        2 - Fabricar coche (a partir del nº de puertas y el nº de plazas)
+                        3 - Fabricar coche (a partir de la marca, el modelo y el color)
+                        4 - Fabricar coche (sin datos)
+                        5 - Tunear coche (pintándolo de un color)
+                        6 - Tunear cohce (sin pintarlo)
+                        7 - Avanzar kilómetros
+                        8 - Mostrar características de un coche
+                        9 - Salir del programa
+                        """));
+                if (option < 1 || option > 9) {
+                    throw  new Exception();
+                }
 
-        //Coche 3
-        Coche car3 = new Coche("BMW", "850", "gris");
-        car3.setMatricula("9012-HH");
-        car3.avanzar(400);
-        car3.setNumPuertas(5);
-
-        //Coche 4
-        Coche car4 = new Coche(5, 7);
-        car4.setMatricula("3456-XS");
-        car4.setMarca("VW");
-        car4.setModelo("Caddy");
-        car4.tunear();
-        car4.avanzar(500);
-
-        Coche[] coches = new Coche[] {
-                car1, car2, car3, car4
-        };
-
-        for (Coche coche : coches) {
-            JOptionPane.showMessageDialog(null, caracteristicas(coche));
-        }
-
+                switch (option) {
+                    case 1 -> {
+                        if (!sitioDisponible()) {
+                            noHaySitioParaOtroCochePorqueElGarajeEsDemasiadoChiquitoPorLoQueNoHaremosOtroCocheQueNosQuiteSitioVital();
+                            throw new Exception();
+                        }
+                        String matricula = JOptionPane.showInputDialog("Indroduce la matrícula");
+                        Coche coche = new Coche(matricula);
+                        JOptionPane.showMessageDialog(null, caracteristicas(coche));
+                    }
+                    case 2 -> {
+                        if (!sitioDisponible()) {
+                            noHaySitioParaOtroCochePorqueElGarajeEsDemasiadoChiquitoPorLoQueNoHaremosOtroCocheQueNosQuiteSitioVital();
+                            throw new Exception();
+                        }
+                        int puertas = Integer.parseInt(JOptionPane.showInputDialog("Introduce el número de puertas"));
+                        int plazas = Integer.parseInt(JOptionPane.showInputDialog("Introduce el número de plazas"));
+                        Coche coche = new Coche(puertas, plazas);
+                        JOptionPane.showMessageDialog(null, caracteristicas(coche));
+                    }
+                    case 3 -> {
+                        if (!sitioDisponible()) {
+                            noHaySitioParaOtroCochePorqueElGarajeEsDemasiadoChiquitoPorLoQueNoHaremosOtroCocheQueNosQuiteSitioVital();
+                            throw new Exception();
+                        }
+                        String marca = JOptionPane.showInputDialog("Indroduce la marca");
+                        String modelo = JOptionPane.showInputDialog("Indroduce el modelo");
+                        String color = JOptionPane.showInputDialog("Indroduce el color");
+                        Coche coche = new Coche(marca, modelo, color);
+                        JOptionPane.showMessageDialog(null, caracteristicas(coche));
+                    }
+                    case 4 -> {
+                        if (!sitioDisponible()) {
+                            noHaySitioParaOtroCochePorqueElGarajeEsDemasiadoChiquitoPorLoQueNoHaremosOtroCocheQueNosQuiteSitioVital();
+                            throw new Exception();
+                        }
+                        Coche coche = new Coche();
+                        JOptionPane.showMessageDialog(null, caracteristicas(coche));
+                    }
+                    case 5 -> {
+                        try {
+                            Coche coche = Coche.buscaCoche(JOptionPane.showInputDialog("Introduce la matrícula del coche"));
+                            String color = JOptionPane.showInputDialog("Elige un color");
+                            coche.tunear(color);
+                        } catch (Exception e) {
+                            JOptionPane.showMessageDialog(null, e.getMessage());
+                        }
+                    }
+                    case 6 -> {
+                        try {
+                            Coche coche = Coche.buscaCoche(JOptionPane.showInputDialog("Introduce la matrícula del coche"));
+                            coche.tunear();
+                        } catch (Exception e) {
+                            JOptionPane.showMessageDialog(null, e.getMessage());
+                        }
+                    }
+                    case 7 -> {
+                        try {
+                            Coche coche = Coche.buscaCoche(JOptionPane.showInputDialog("Introduce la matrícula del coche"));
+                            coche.avanzar(Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos km?")));
+                        } catch (Exception e) {
+                            JOptionPane.showMessageDialog(null, e.getMessage());
+                        }
+                    }
+                    case 8 -> {
+                        try {
+                            Coche coche = Coche.buscaCoche(JOptionPane.showInputDialog("Introduce la matrícula del coche"));
+                            JOptionPane.showMessageDialog(null, caracteristicas(coche));
+                        } catch (Exception e) {
+                            JOptionPane.showMessageDialog(null, e.getMessage());
+                        }
+                    }
+                    case 9 -> {
+                        JOptionPane.showMessageDialog(null, "Ten un buen día, tarde, noche, o lo que proceda");
+                    }
+                }
+            } catch (Exception e) {}
+        } while (option != 9);
     }
 
+    /**
+     * Comprueba si hay sitio disponible en el garaje
+     * @return
+     */
+    public static boolean sitioDisponible() {
+        return Coche.numcoches < Coche.MAX_COCHES;
+    }
+
+    /**
+     * Informa al usuario de que no hay sitio para más coches porque tiene un garaje pequeñito
+     */
+    public static void noHaySitioParaOtroCochePorqueElGarajeEsDemasiadoChiquitoPorLoQueNoHaremosOtroCocheQueNosQuiteSitioVital() {
+        JOptionPane.showMessageDialog(null, """
+                No hay sitio para más coches.
+                Puedes comprar una ampliación
+                por solo 9'95.
+                """);
+    }
+
+    /**
+     * Muestra las caracterçisticas de un coche
+     * @param coche el coche
+     * @return las características
+     */
     public static String caracteristicas(Coche coche) {
         return new StringBuilder()
                 .append("Matrícula: ").append(coche.getMatricula()).append("\n")
